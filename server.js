2d2581c14b8d7f5eed1c7f77b3435ca3ab9e2124
Cyril Marcus <brownkcing@gmail.com>
2021-03-16 15:01:19 +0800

    test

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "client", "build")))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "client", "build"))
 })
 
 const db = require("./server/models");
