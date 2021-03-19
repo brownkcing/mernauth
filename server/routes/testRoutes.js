81f23484803ca628ba1043ac109382f6cd6674a4
Cyril Marcus <brownkcing@gmail.com>
2021-03-19 17:14:15 +0800

    routes test 4

--- server/routes/testRoutes.js
+++ server/routes/testRoutes.js
@@ -4,7 +4,7 @@ const routes = require('express').Router();
 
 
 module.exports = function(app) {
-    routes.get('/', (req, res) => {
+    routes.get('api/test/test2', (req, res) => {
         res.status(200).json({ message: 'Connected!' });
       });
 }
