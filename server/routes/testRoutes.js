ff472d90aa15560c68447c6fa89067b31004803c
Cyril Marcus <brownkcing@gmail.com>
2021-03-19 17:27:33 +0800

    routes test 7

--- server/routes/testRoutes.js
+++ server/routes/testRoutes.js
@@ -1,10 +1,11 @@
-const { authJwt } = require("../middlewares");
-const controller = require("../controllers/user.controller");
-const routes = require('express').Router();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
 
 
 module.exports = function(app) {
-    app.get('api/test/test2', (req, res) => {
+    app.get('/test2', (req, res) => {
         res.status(200).json({ message: 'Connected!' });
       });
 }
