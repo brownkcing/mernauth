091edd49aa49b0acd7f37dbdf693ff1251da350f
Cyril Marcus <brownkcing@gmail.com>
2021-03-20 00:07:07 +0800

    frontend-backend connect attemp 2

--- src/home/Login.js
+++ src/home/Login.js
@@ -1,7 +1,6 @@
 import React, {useState, useRef} from 'react';
 import styled from 'styled-components';
 import * as Mixins from '../styles/Mixins';
-import {Link} from 'react-router-dom';
 import Form from 'react-validation/build/form';
 import Input from 'react-validation/build/input';
 import CheckButton from 'react-validation/build/button';
@@ -79,7 +78,7 @@ const Login = (props) => {
         if (checkBtn.current.context._errors.length === 0) {
           AuthService.login(username, password).then(
             () => {
-              props.history.push("/User");
+              props.history.push("/user");
               window.location.reload();
             },
             (error) => {
