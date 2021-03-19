0c2537d8145bc8ca581e5bb62476681c7e9e6d11
Cyril Marcus <brownkcing@gmail.com>
2021-03-19 19:28:19 +0800

    test routes 19

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,11 +3,20 @@ const controller = require("../controllers/user.controller");
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
-    res.set({
-      "Access-Control-Allow-Headers":
-      "x-access-token, Origin, Content-Type, Accept",
-      "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE"
-    })
+
+
+    res.setHeader('Access-Control-Allow-Origin', 'https://mernauthbackend.herokuapp.com/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, x-access-token, Origin, Content-Type, Accept');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
   });
 
   app.get('/all', (req, res) => {
