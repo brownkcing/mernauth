bfa63716463190dac87f20244a28cf18e9f6c04b
Cyril Marcus <brownkcing@gmail.com>
2021-03-19 19:12:36 +0800

    test routes 15

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,9 +3,11 @@ const controller = require("../controllers/user.controller");
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.set({
+      "Access-Control-Allow-Headers":
+      "x-access-token, Origin, Content-Type, Accept",
+      "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE"
+    })
     next();
   });
 
