0d721c52d28a83da3ad1e6183332207ff54f7a91
Cyril Marcus <brownkcing@gmail.com>
2021-03-22 22:46:40 +0800

    testing auth0 11

--- src/home/Auth0/AuthProvider.js
+++ src/home/Auth0/AuthProvider.js
@@ -13,12 +13,13 @@ const Auth0ProviderWithHistory = ({ children }) => {
   const onRedirectCallback = (appState) => {
     history.push(appState?.returnTo || window.location.pathname);
   };
+  const urired =  `${window.location.origin}/profile`
 
   return (
     <Auth0Provider
       domain="dev-1ylkmsz0.us.auth0.com"
       clientId="FbqCvqDiQIXMBOoOEbj96Lgvzkfvb1rg"
-      redirectUri={window.location.origin}
+      redirect_uri= {window.location.origin + "/profile"} 
       audience="https://dev-1ylkmsz0.us.auth0.com/api/v2/"
       scope="read:current_user update:current_user_metadata"
     >
