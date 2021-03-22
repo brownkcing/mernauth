b0d8d0325f0afbd12a14b3f47899561075fe672f
Cyril Marcus <brownkcing@gmail.com>
2021-03-22 19:29:22 +0800

    testing auth0 test1

--- src/home/Auth0/Auth0Button.js
+++ src/home/Auth0/Auth0Button.js
@@ -5,11 +5,3 @@ import LogoutButton from "./logout-button";
 
 import { useAuth0 } from "@auth0/auth0-react";
 
-
-const AuthenticationButton = () => {
-    const { isAuthenticated } = useAuth0();
-  
-    return isAuthenticated ? <LogoutButton /> : <LoginButton />;
-  };
-  
-  export default AuthenticationButton;
