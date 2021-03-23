b6e5608fc0bbe64a09ce5540fc8113f5ba92f9c5
Cyril Marcus <brownkcing@gmail.com>
2021-03-23 10:54:54 +0800

    testing auth0 12

--- src/home/Auth0/AuthProvider.js
+++ src/home/Auth0/AuthProvider.js
@@ -13,13 +13,12 @@ const Auth0ProviderWithHistory = ({ children }) => {
   const onRedirectCallback = (appState) => {
     history.push(appState?.returnTo || window.location.pathname);
   };
-  const urired =  `${window.location.origin}/profile`
 
   return (
     <Auth0Provider
       domain="dev-1ylkmsz0.us.auth0.com"
       clientId="FbqCvqDiQIXMBOoOEbj96Lgvzkfvb1rg"
-      redirect_uri= {window.location.origin + "/profile"} 
+      redirect_uri= "https://mernauthfrontend.netlify.app/profile&"
       audience="https://dev-1ylkmsz0.us.auth0.com/api/v2/"
       scope="read:current_user update:current_user_metadata"
     >
