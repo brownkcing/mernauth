2e9a053beaf3eacc0eec64b45bf3df596aa6243c
Cyril Marcus <brownkcing@gmail.com>
2021-03-23 16:41:34 +0800

    rendering profile.js for either authentication test 4

--- src/home/Auth0/Auth0Protected.js
+++ src/home/Auth0/Auth0Protected.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
+const userLogged = AuthService.getCurrentUser();
 
 const ProtectedRoute = ({ component, ...args }) => (
+  <React.Fragment>
+  {userLogged != null ? null : 
   <Route
     component={withAuthenticationRequired(component)}
     {...args}
-  />
+  />}
+  
+  </React.Fragment>
 );
 
 export default ProtectedRoute;
